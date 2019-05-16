932be8e3785d2d964bd753f6405346531d221ccc
Sallem Ahmed <asallem94@gmail.com>
2019-05-16 15:01:08 -0400

    set up react

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -9,8 +9,8 @@ import { HashRouter } from 'react-router-dom';
 const Root = () => {
   return (
     <HashRouter>
-        <App/>
-      </HashRouter>
+      <App/>
+    </HashRouter>
   )
   // <Provider store={store}>
   // </Provider>
